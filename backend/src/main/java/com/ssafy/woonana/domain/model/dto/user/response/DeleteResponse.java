e343b89beb50c079ee2ef3cd36b2f927b0b0d894
배나영 <qoskdud15@daum.net>
2022-04-26 16:23:01 +0900

    feat: 🎸 회원가입, 로그인 기능 구현

---
+++ backend/src/main/java/com/ssafy/woonana/domain/model/dto/user/response/DeleteResponse.java
@@ -0,0 +1,16 @@
+package com.ssafy.woonana.domain.model.dto.user.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DeleteResponse {
+
+    private String delete;
+
+}

2446b408669e95f7028dec216b2c209f50761401
정채은 <ddj03340@naver.com>
2022-04-26 17:08:24 +0900

    feat: 🎸 auditing 설정

--- backend/src/main/java/com/ssafy/woonana/WoonanaApplication.java
+++ backend/src/main/java/com/ssafy/woonana/WoonanaApplication.java
@@ -2,9 +2,16 @@ package com.ssafy.woonana;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+import java.util.UUID;
 
 // http://localhost:8081/swagger-ui/index.html
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class WoonanaApplication {
 
@@ -12,4 +19,15 @@ public class WoonanaApplication {
 		SpringApplication.run(WoonanaApplication.class, args);
 	}
 
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		return new AuditorAware<String>() {
+			@Override
+			public Optional<String> getCurrentAuditor() {
+				return Optional.of(UUID.randomUUID().toString());
+			}
+		};
+
+//		return () -> Optional.of(UUID.randomUUID().toString());
+	}
 }

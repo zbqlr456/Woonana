9b886bd47fbd3fa2e53476da87e9856ba92a522e
zbqlr456 <zbqlr456@naver.com>
2022-04-26 16:26:24 +0900

    feat: 🎸 채팅방 입장, 채팅 전송기능 추가 ver.1

---
+++ backend/src/main/java/com/ssafy/woonana/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package com.ssafy.woonana.config;
+
+import com.ssafy.woonana.Handler.WebSocketHandler;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final WebSocketHandler webSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
+        registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
+    }
+
+}
--- backend/src/main/java/com/ssafy/woonana/controller/chat/ChatController.java
+++
@@ -1,4 +0,0 @@
-package com.ssafy.woonana.controller.chat;
-
-public class ChatController {
-}

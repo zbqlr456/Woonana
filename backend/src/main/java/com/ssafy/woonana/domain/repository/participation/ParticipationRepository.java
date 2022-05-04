3ee27f2861d2da806922b221910d965014c43005
qoskdud15 <qoskdud15@daum.net>
2022-05-04 11:38:56 +0900

    feat: 🎸 평가하기, 참여내역 개발

--- backend/src/main/java/com/ssafy/woonana/domain/repository/participation/ParticipationRepository.java
+++ backend/src/main/java/com/ssafy/woonana/domain/repository/participation/ParticipationRepository.java
@@ -1,7 +1,15 @@
 package com.ssafy.woonana.domain.repository.participation;
 
 import com.ssafy.woonana.domain.model.entity.participation.Participation;
+import com.ssafy.woonana.domain.model.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ParticipationRepository extends JpaRepository<Participation, Long> {
+
+    List<Participation> findParticipationsByUser(User user); // user에 해당하는 참여들 리턴
+
+
 }

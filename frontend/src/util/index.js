6531f6d24fe76354aea43f89efb855de06030a8b
unknown <ufoyws@naver.com>
2022-05-17 22:46:35 +0900

    test: 💍 로그인 테스트

--- frontend/src/util/index.js
+++ frontend/src/util/index.js
@@ -3,10 +3,6 @@ import axios from "axios";
 // axios 객체 생성
 export default axios.create({
   withCredentials: false,
-<<<<<<< HEAD
-  // baseURL: "http://localhost:8081",
-=======
->>>>>>> e47531e7abd82cf5203ef106190ce533375e49bd
   baseURL: process.env.VUE_APP_BACK_URI,
   headers: {
     "Content-type": "application/json",

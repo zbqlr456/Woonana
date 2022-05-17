53fc81bb6b6d566521204b7a5d5ffaca15a7f563
zbqlr456 <zbqlr456@naver.com>
2022-05-17 20:51:55 +0900

    fix: 🐛 vue 컨벤션 정리

---
+++ frontend/.history/src/components/Blogs/index_20220517203243.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div v-if="showloading">
+      <loader-appie />
+    </div>
+    <!-- Blog Start -->
+    <section class="blogpage-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 col-md-7">
+            <all-blogs />
+          </div>
+        </div>
+      </div>
+    </section>
+    <footer-home-one />
+  </div>
+</template>
+
+<script>
+import FooterHomeOne from "../HomeOne/FooterHomeOne.vue";
+import LoaderAppie from "../LoaderAppie.vue";
+import AllBlogs from "./AllBlogs.vue";
+export default {
+  components: {
+    LoaderAppie,
+    FooterHomeOne,
+    AllBlogs,
+  },
+  created() {
+    if (!this.rtl) {
+      this.preLoading();
+    } else {
+      this.showloading = false;
+    }
+  },
+
+  methods: {
+    preLoading() {
+      setTimeout(() => {
+        this.showloading = false;
+      }, 2000);
+    },
+  },
+  data() {
+    return {
+      showloading: true,
+    };
+  },
+};
+</script>
+
+<style></style>

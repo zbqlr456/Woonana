a88b59d7e876b47037bd649145b352c2ecbfe7ae
zbqlr456 <zbqlr456@naver.com>
2022-05-19 02:08:16 +0900

    마이페이지 메인 조금 수정

--- frontend/.history/src/components/Mypage/MypageMain_20220519015042.vue
+++ frontend/.history/src/components/Mypage/MypageMain_20220519015042.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="mypage-container">
     <div class="mypage_content">
-      <div id="profile-weather">
-        <div id="profile">
-          <div class="col-md-1" id="profile-img-box">
-            <b-img id="profile-img" v-bind:src="myinfomation.userProfileUrl"></b-img>
-          </div>
-          <div class="col-md-1">
-            <p id="profile-nickname">{{ myinfomation.userNickname }}</p>
-          </div>
+      <div class="profile" id="profile">
+        <div class="col-md-1" id="profile-img-box">
+          <b-img id="profile-img" v-bind:src="myinfomation.userProfileUrl"></b-img>
         </div>
-        <div id="my-weather">
-          <p id="weather-list">
-            {{ myinfomation.userNickname }}님의 현재 날씨 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <div class="col-md-1">
+          <p id="profile-nickname">{{ myinfomation.userNickname }}</p>
+          <p>
+            {{ myinfomation.userNickname }}님의 날씨 :
             <i v-if="this.myrating <= 1" class="fas fa-bolt"></i>
             <i v-if="this.myrating <= 2 && this.myrating > 1" class="fas fa-cloud-rain"></i>
             <i v-if="this.myrating <= 3 && this.myrating > 2" class="fas fa-cloud"></i>
@@ -21,24 +17,23 @@
           </p>
         </div>
       </div>
+
       <div id="my-promise">
         <div>나의 약속</div>
         <div>
-          <span v-for="(appoint, index) in myappointment" :key="index" class="badge bg-black">{{
+          <span v-for="(appoint, index) in myappointment" :key="index" class="badge bg-secondary">{{
             appoint.boardMeetStartDate
           }}</span>
         </div>
       </div>
-      <div id="promise-list">
-        <div>모집 진행 중인 모임</div>
-        <!-- 버튼 모임 유무에따라 활성화, 비활성화 할것!!! -->
-        <b-button v-show="checkBoardid" @click="movepost()"
-          >바로가기&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;></b-button
-        >
-        <b-button v-show="!checkBoardid" disabled
-          >바로가기&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;></b-button
-        >
-      </div>
+      <div>모집 진행 중인 모임</div>
+      <!-- 버튼 모임 유무에따라 활성화, 비활성화 할것!!! -->
+      <b-button v-show="checkBoardid" @click="movepost()"
+        >바로가기&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;></b-button
+      >
+      <b-button v-show="!checkBoardid" disabled
+        >바로가기&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;></b-button
+      >
     </div>
     <ul class="list-group list-group-flush">
       <li class="my-page-list list-group-item">
@@ -166,17 +161,18 @@ export default {
 .mypage_content {
   margin: 1%;
 }
-#profile-weather {
-  margin-top: 60px;
+#profile-and-rating {
   display: flex;
-  justify-content: flex-start;
-  align-items: center;
+  align-items: baseline;
+}
+.profile {
+  height: 100%;
 }
 #profile {
-  height: 100%;
   display: flex;
   justify-content: flex-start;
   flex-direction: column;
+  margin-top: 60px;
   margin-bottom: 10px;
 }
 #profile-img-box {
@@ -193,23 +189,9 @@ export default {
   object-fit: cover;
 }
 #profile-nickname {
-  font-weight: 900;
-  font-size: 12px;
+  font-weight: bold;
   margin-top: 3px;
-  margin-left: 20px;
-}
-#my-weather {
-  margin-left: 6%;
-  margin-bottom: 8%;
-}
-#weather-list {
-  display: flex;
-  align-items: center;
-  font-weight: 900;
-  font-size: 18px;
-}
-.fas {
-  font-size: 36px;
+  margin-left: 14px;
 }
 #my-promise {
   font-weight: bold;

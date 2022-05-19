eafa54e413ea7118e734b7e213033b7ae250bce2
unknown <ufoyws@naver.com>
2022-05-20 02:33:42 +0900

    fix: 🐛 마이페이지 내 참여목록, 내글목록 게시글 연결 버그 수정 및 css 조정

--- frontend/src/components/Blogs/SingleBlog.vue
+++ frontend/src/components/Blogs/SingleBlog.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="single-post-area Jua">
     <blockquote>
+      <h4 class="article-title Jua">{{ board.title }}</h4>
       <cite v-if="board.participationOption == 0">모집형태 => 신청제</cite>
       <cite v-if="board.participationOption == 1">모집형태 => 선착순</cite>
-      <br />
-      <h4 class="article-title Jua">{{ board.title }}</h4>
-
       <p>작성자 : {{ board.userNickname }}</p>
       <cite>현재 인원 : ({{ board.allowedNumber }}/{{ board.maxNumber }})</cite>
       <div class="post-thumb">
@@ -19,16 +17,30 @@
         <a v-if="board.exerciseId == 3">#캐치볼</a>
         <a v-if="board.exerciseId == 4">#산책</a>
       </div>
-
-      <a> 만남 장소 : {{ board.place }} </a>
-      <a>
-        만남 시작 시간 : {{ board.meetStartDate.slice(0, 10) }}
-        {{ board.meetStartDate.slice(11, 16) }}
-      </a>
-      <a>
-        만남 종료 시간 : {{ board.meetEndDate.slice(0, 10) }} {{ board.meetEndDate.slice(11, 16) }}
-      </a>
       <hr />
+      <div>
+        <h5 class="Jua">만남 장소</h5>
+        <a>{{ board.place }} </a>
+      </div>
+      <div>
+        <h5 class="Jua">만날 시간</h5>
+        <a>
+          {{ board.meetStartDate.slice(0, 10) }}
+          {{ board.meetStartDate.slice(11, 16) }}
+        </a>
+      </div>
+      <div>
+        <h5 class="Jua">헤어질 시간</h5>
+        <a>
+          {{ board.meetEndDate.slice(0, 10) }}
+          {{ board.meetEndDate.slice(11, 16) }}
+        </a>
+      </div>
+      <hr />
+      <h5 class="Jua">참여</h5>
+      <a v-if="this.board.userNickname == myinfomation.userNickname"
+        >방장은 신청하기 버튼이 비활성화 됩니다.</a
+      >
       <div
         v-if="
           this.board.userNickname != myinfomation.userNickname &&
@@ -36,24 +48,50 @@
         "
         class="post-tags"
       >
-        <h5 class="Jua">참여하기 =></h5>
-        <a @click="join()">신청하기</a>
+        <button
+          @click="join()"
+          class="btn btn-light Jua"
+          style="background-color: #be81f7"
+        >
+          신청하기
+        </button>
       </div>
-      <br />
+      <hr />
+      <h5 class="Jua">멤버목록</h5>
       <div class="post-tags">
-        <h5 class="Jua">멤버목록 =></h5>
-        <a
+        <button
           v-if="this.board.userNickname == myinfomation.userNickname"
           @click="viewParticipatedMember()"
-          >신청자 조회</a
+          class="btn btn-light Jua"
+          style="background-color: #2e9afe"
         >
-        <a @click="viewMember()">멤버조회</a>
+          신청자 조회
+        </button>
+        <button
+          @click="viewMember()"
+          class="btn btn-light Jua"
+          style="background-color: #ffbf00"
+        >
+          멤버조회
+        </button>
       </div>
-      <br />
-      <div v-if="this.board.userNickname == myinfomation.userNickname" class="post-tags">
+      <hr />
+      <h5 class="Jua">게시글 삭제</h5>
+      <a v-if="this.board.userNickname != myinfomation.userNickname"
+        >글 작성자만 삭제할 수 있습니다.</a
+      >
+      <div
+        v-if="this.board.userNickname == myinfomation.userNickname"
+        class="post-tags"
+      >
         <br />
-        <h5 class="Jua">글 삭제하기 =></h5>
-        <a @click="blogdelete()">삭제하기</a>
+        <button
+          class="btn btn-light Jua"
+          @click="blogdelete()"
+          style="background-color: #ff0000"
+        >
+          삭제하기
+        </button>
       </div>
     </blockquote>
   </div>
@@ -97,7 +135,8 @@ export default {
           this.$router.go();
         })
         .catch((err) => {
-          let msg = "이미 신청하셨거나, 신청이 불가한 상태입니다. 다시 확인해 주세요";
+          let msg =
+            "이미 신청하셨거나, 신청이 불가한 상태입니다. 다시 확인해 주세요";
           console.log(err);
           alert(msg);
         });
@@ -118,7 +157,11 @@ export default {
     viewMember: function () {
       this.$router.push({
         name: "MemberList",
-        query: { data: this.boardId, title: this.board.title, boardImg: this.board.imageUrl },
+        query: {
+          data: this.boardId,
+          title: this.board.title,
+          boardImg: this.board.imageUrl,
+        },
       });
     },
     viewParticipatedMember: function () {
